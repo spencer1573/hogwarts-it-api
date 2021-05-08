371ee7b55c1c6609b35b4436bb6007d36656ed9e
spencer1573 <spencer1573@users.noreply.github.com>
2021-05-08 15:39:10 -0600

    moved interfaces

--- src/controllers/createStudent.ts
+++ src/controllers/createStudent.ts
@@ -1,23 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import pool from '../database/db'
 
-interface iStudentRaw {
-  id: string
-  studentPublicId: string
-  email: string
-  firstName: string
-  middleName: string
-  lastName: string
-}
-
-interface iRawGrades {
-  id: string
-  courseId: string
-  studentId: string
-  semesterId: string
-  grade: number
-}
-
 const createStudent = async (
   req: Request,
   res: Response,
